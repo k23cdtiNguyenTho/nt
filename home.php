2bad015a00120c60b4feaf93344352c9c4de66e6
k23cdtiNguyenTho <nguynth7@gmail.com>
2024-10-30 07:38:51 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,345 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>WEBSITE NGUYỄN THỌ</title>
+    <meta CHARSET="UTF-8">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="main-1.css">
+    <link rel="stylesheet" href="main-2.css">
+    <link rel="stylesheet" href="main-3.css">
+    <link rel="stylesheet" href="footer.css">
+</head>
+
+<body>
+<! PHẦN HEADER >
+<header>
+        <div class="menu-icon">
+            <i class="fas fa-bars"></i>
+        </div>
+        <div class="nav-links">
+            <a href="#">HOMES <i class="fas fa-caret-down"></i></a>
+            <a href="#">FEATURES <i class="fas fa-caret-down"></i></a>
+            <a href="#">PAGES <i class="fas fa-caret-down"></i></a>
+        </div>
+        <div class="logo">
+            AUTEUR
+            <div class="gach">
+                <div class="duonggach"></div>
+                <div class="icon-nho"><i class="fas fa-star-of-life"></i></div>
+                <div class="duonggach"></div>
+            </div>
+            <p>AUTHOR & WRITER</p>
+        </div>
+        <div class="nav-links">
+            <a href="#">SHOP <i class="fas fa-caret-down"></i></a>
+            <a href="#">EVENT</a>
+            <a href="#">BLOG <i class="fas fa-caret-down"></i></a>
+        </div>
+        <div class="icons">
+            <i class="fas fa-search"></i>
+            <div class="icon-mini">
+                <i class="fas fa-shopping-cart"></i>
+                <div class="soluong">0</div>
+            </div>
+            <i class="fas fa-user"></i>
+        </div>  
+</header>
+<! KẾT THÚC PHẦN HEADER>
+<!--------------------------------------------------->
+<! PHẦN MAIN 1>
+<main class="main-1">
+        <div class="left">
+         <p>FOR AUTHOR & WRITE</p>
+         <h1>Meet Your Next</h1>
+         <h2>Online course</h2>
+         <div class="logo">
+            <img src="https://storage.googleapis.com/a1aa/image/xxMcsLW5Fl6oCtv2W3Ztiymi3MSv80MvDPpDtHsfSImQPmzJA.jpg" />
+            <img src="https://storage.googleapis.com/a1aa/image/uTyxbazzvPplE54ZQpL8y2YeepYsPg4yEXn4qdmuMDiceYOnA.jpg" />
+            <img src="https://storage.googleapis.com/a1aa/image/zZNeSgOajepgvE53Prlf99UVrdrP06QNEG2CfTGgg1H25xcOB.jpg" />
+         </div>
+         <button>
+          PURCHASE
+         </button>
+        </div>
+        <div class="right"> 
+            <img class="zoom" id="img1" src="img/young-man-using-desktop-computer-sitting-in-chair-with-desk-free-vector.jpg" />
+            <img class="zoom" id="img1" src="img/young-man-using-desktop-computer-sitting-in-chair-with-desk-free-vector.jpg " />
+            <img class="zoom" id="img2" src="img/woman.jpg" />
+            <img class="zoom" id="img2" src="img/woman.jpg" />
+        </div>
+</main>
+<! KẾT THÚC PHẦN MAIN 1>
+<!--------------------------------------------------->
+<!--------------------------------------------------->
+<! PHẦN MAIN 2>
+<main class="main-2">
+    <div class="header">  
+        <img src="img/A.png"/>
+        <h1>Welcome to Auteur</h1>
+    </div>
+    <div class="courses">
+        <div class="course">
+         <i id="cloud" class="fas fa-cloud"></i>
+         <p>IT</p>
+         <p>1,160 Courses <a href="#"><i id="muiten" class="fas fa-arrow-right"></i></a></p>
+        </div>
+
+        <div class="course">
+         <i id="health" class="fas fa-heartbeat"></i>
+         <p>Health</p>
+         <p>935 Courses <a href="#"><i id="muiten" class="fas fa-arrow-right"></i></a></p>
+        </div>
+
+        <div class="course">
+         <i id="language" class="fas fa-language"></i>
+         <p>Language</p>
+         <p>310 Courses <i id="muiten" class="fas fa-arrow-right"></i></a></p>
+        </div>
+
+        <div class="course">
+         <i id="business" class="fas fa-chart-line"></i>
+         <p>Business</p>
+         <p>1,604 Courses <a href="#"><i id="muiten" class="fas fa-arrow-right"></i></a></p>
+        </div>
+
+        <div class="course">
+         <i id="management" class="fas fa-users"></i>
+         <p>Management</p>
+         <p>956 Courses <a href="#"><i id="muiten" class="fas fa-arrow-right"></i></a></p>
+        </div>
+
+        <div class="course">
+         <i id="english" class="fas fa-book"></i>
+         <p>English</p>
+         <p>55 Courses <a href="#"><i id="muiten" class="fas fa-arrow-right"></i></a></p>
+        </div>
+
+        <div class="course">
+         <i id="personal" class="fas fa-universal-access"></i>
+         <p>Personal Development</p>
+         <p>1,241 Courses <a href="#"><i id="muiten" class="fas fa-arrow-right"></i></a></p>
+        </div>
+
+        <div class="course">
+         <i id="sale" class="fas fa-bullhorn"></i>
+         <p>Sales & <br> Marketing</p>
+         <p>413 Courses <a href="#"><i id="muiten" class="fas fa-arrow-right"></i></a></p>
+        </div>
+
+        <div class="course">
+         <i id='engineer' class="fas fa-hard-hat"></i>
+         <p>Engineering &<br> Construction</p>
+         <p>780 Courses <a href="#"><i id="muiten" class="fas fa-arrow-right"></i></a>
+         </p>
+        </div>
+
+        <div class="course">
+         <i id="teaching" class="fas fa-graduation-cap"></i>
+         <p>Teaching &<br> Academics</p>
+         <p>1,504 Courses<a href="#"><i id="muiten" class="fas fa-arrow-right"></i></a>
+         </p>
+        </div>
+        
+    </div>
+</main>
+<! KẾT THÚC PHẦN MAIN 2>
+<!--------------------------------------------------->
+<!--------------------------------------------------->
+<! PHẦN MAIN 3>
+<main class="main-3">
+        <div class="muiten">
+        <a href="#"><i class="fas fa-chevron-left"></i></a>
+        </div>
+
+        <div class="the">
+         <div class="certificate"><p>CERTIFICATE</p></div>
+         <img height="150" width="250" src="https://storage.googleapis.com/a1aa/image/R1IKeftaN1m6103eGgl3eBAkchq3wIhOSyPFQ07gTDAn3AhOB.jpg"/>
+         <div class="the-body">
+          <p>Health</p>
+          <h5>Caregiving Skills - Dementia Care</h5><br><br>
+          <div class="the-tg">
+           <i class="far fa-clock"></i>
+           <p>2 - 3 hrs</p>
+           <span>&nbsp;&nbsp;&nbsp;</span>
+           <i class="fas fa-user-graduate"></i>
+           <p>291,138 learners</p>
+          </div>
+         </div>
+         <div class="the-footer">
+            <button id="button1">More Info</button>
+            <button id="button2">Start Learning</button>
+            </div>
+        </div>
+
+        <div class="the">
+            <div class="certificate"><p>CERTIFICATE</p></div>
+            <img height="150" width="250" src="https://storage.googleapis.com/a1aa/image/R1IKeftaN1m6103eGgl3eBAkchq3wIhOSyPFQ07gTDAn3AhOB.jpg"/>
+            <div class="the-body">
+             <p>Health</p>
+             <h5>Caregiving Skills - Dementia Care</h5><br><br>
+             <div class="the-tg">
+              <i class="far fa-clock"></i>
+              <p>2 - 3 hrs</p>
+              <span>&nbsp;&nbsp;&nbsp;</span>
+              <i class="fas fa-user-graduate"></i>
+              <p>291,138 learners</p>
+             </div>
+            </div>
+            <div class="the-footer">
+                <button id="button1">More Info</button>
+                <button id="button2">Start Learning</button>
+                </div>
+           </div>
+
+           <div class="the">
+            <div class="certificate"><p>CERTIFICATE</p></div>
+            <img height="150" width="250" src="https://storage.googleapis.com/a1aa/image/R1IKeftaN1m6103eGgl3eBAkchq3wIhOSyPFQ07gTDAn3AhOB.jpg"/>
+            <div class="the-body">
+             <p>Health</p>
+             <h5>Caregiving Skills - Dementia Care</h5><br><br>
+             <div class="the-tg">
+              <i class="far fa-clock"></i>
+              <p>2 - 3 hrs</p>
+              <span>&nbsp;&nbsp;&nbsp;</span>
+              <i class="fas fa-user-graduate"></i>
+              <p>291,138 learners</p>
+             </div>
+            </div>
+            <div class="the-footer">
+                <button id="button1">More Info</button>
+                <button id="button2">Start Learning</button>
+                </div>
+           </div>
+
+           <div class="the">
+            <div class="certificate"><p>CERTIFICATE</p></div>
+            <img height="150" width="250" src="https://storage.googleapis.com/a1aa/image/R1IKeftaN1m6103eGgl3eBAkchq3wIhOSyPFQ07gTDAn3AhOB.jpg"/>
+            <div class="the-body">
+             <p>Health</p>
+             <h5>Caregiving Skills - Dementia Care</h5><br><br>
+             <div class="the-tg">
+              <i class="far fa-clock"></i>
+              <p>2 - 3 hrs</p>
+              <span>&nbsp;&nbsp;&nbsp;</span>
+              <i class="fas fa-user-graduate"></i>
+              <p>291,138 learners</p>
+             </div>
+            </div>
+            <div class="the-footer">
+            <button id="button1">More Info</button>
+            <button id="button2">Start Learning</button>
+            </div>
+           </div>
+
+           <div class="muiten">
+            <a href="#"><i class="fas fa-chevron-right"></i></a>
+            </div>
+</main>
+<! KẾT THÚC PHẦN MAIN 3>
+<!--------------------------------------------------->
+<!--------------------------------------------------->
+<! PHẦN FOOTER>
+
+<footer>
+    <div class="line"></div>
+
+    <div class="ct">
+     <div class="footer-left">
+      <table>
+        <tr>
+          <td>
+            <div class="container">
+                <div class="logo">
+                    <img src="img/logo_footer-removebg-preview.png"/>
+                <div class="logo-text">
+                    Alison
+                </div>
+                </div>
+                <div class="tagline">
+                    EMPOWER YOURSELF
+                </div>  
+            </div>  
+          </td>
+
+         <td>
+            <ul>
+                <li><i id="kci" class="fas fa-question-circle"></i>FAQs</li>
+                <li><i id="kci" class="fas fa-question-circle"></i>Customer Support</li>
+            </ul>
+         </td>
+        </tr>
+
+     </table>
+     <div class="footer-bottom">
+        <img src="img/facebook.png"/>
+        <img src="img/X.png"/>
+        <img src="img/linkedin.png"/>
+        <img src="img/instagram.png"/>
+        <img src="img/tiktok.png"/>
+        <img src="img/youtube.png"/>
+        <img src="img/quora.png"/>
+        <img src="img/telegram.png"/>
+      </div>
+     </div>
+
+    
+<!------------------------------------------------------------------------------->
+    <div class="footer-right">
+
+        <div class="top">
+            <div class="left">
+            <span>Rated Excellent</span>      
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half"></i>
+            </div>
+            <div class="right">
+                <i class="fas fa-star"></i>
+                <span>Trustpilot</span>
+            </div>
+        </div>
+        <div class="app">
+        <div class="store">
+            <img src="img/iconfb.png"/>
+            <div class="text">
+             <div class="small-text">
+              Download on the
+             </div>
+             <div class="large-text">
+              App Store
+             </div>
+            </div>
+        </div>
+        <div class="store">
+            <img src="img/iconfb.png"/>
+             <div class="text">
+                <div class="small-text">
+                     Download on the
+                </div>
+                <div class="large-text">
+                    App Store
+                </div>
+             </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>© Alison 2024</p>
+            <p>Privacy</p>
+            <p>Terms</p>
+            <p>Cookie Policy</p>
+            <p>Sitemap</p>
+        </div>
+
+     </div>
+    </div>
+<!------------------------------------------------------------------------------->
+</footer>
+
+</body>
+
+</html>
